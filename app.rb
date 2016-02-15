982e0845d4e599f61e06d930ce611feb93ec0724
michal-kazmierczak <michal.kazmierczak@netguru.co>
2016-02-15 23:35:51 +0100

    add simple logging

--- app.rb
+++ app.rb
@@ -5,6 +5,13 @@ require "pry" if Sinatra::Base.development?
 class App < Sinatra::Base
   register Sinatra::ActiveRecordExtension
 
+  configure do
+    enable :logging
+    file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+    file.sync = true
+    use Rack::CommonLogger, file
+  end
+
   before do
     content_type :json
   end
