5ececec79a6cc4204209e998b4be93b418719334
michal-kazmierczak <michal.kazmierczak@netguru.co>
2016-02-21 03:37:09 +0100

    assign assign_day_of_week before save entry

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -12,7 +12,16 @@
 #
 
 class Entry < ActiveRecord::Base
+  before_create :assign_day_of_week
+
   validates :swimmers, presence: true, numericality: true
   validates :date, presence: true
   validates :device_id, length: { maximum: 40 }
+
+  private
+
+  def assign_day_of_week
+    binding.pry
+    self.day_of_week = date.wday
+  end
 end
