4736be4fadce0444e93b0d97407425f023787831
michal-kazmierczak <michal.kazmierczak@netguru.co>
2016-03-07 23:15:48 +0100

    better naming for env variable

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,3 @@
-server ENV["PRODUCTION_SERVER"], user: ENV["PRODUCTION_USER"], roles: %w(app db web)
+server ENV["STAGING_SERVER"], user: ENV["STAGING_USER"], roles: %w(app db web)
 
 set :application, 'pool_stats_staging'
